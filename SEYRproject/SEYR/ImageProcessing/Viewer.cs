cfdfaedd37a47f23a532d5c44b0210b66adf480f
Brad Martin <bradmartin333@gmail.com>
2022-04-14 08:16:19 -0400

    codefactor

--- SEYRproject/SEYR/ImageProcessing/Viewer.cs
+++ SEYRproject/SEYR/ImageProcessing/Viewer.cs
@@ -17,16 +17,13 @@ namespace SEYR.ImageProcessing
         {
             if (hue == 360) return Color.FromArgb(255, Color.White);
             if (hue == 0) return Color.FromArgb(255, Color.Black);
-
             int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
             double f = (hue / 60) - Math.Floor(hue / 60);
-
             value *= 255;
             int v = Convert.ToInt32(value);
             int p = Convert.ToInt32(value * (1 - saturation));
             int q = Convert.ToInt32(value * (1 - (f * saturation)));
             int t = Convert.ToInt32(value * (1 - ((1 - f) * saturation)));
-
             if (hi == 0)
                 return Color.FromArgb(255, v, t, p);
             else if (hi == 1)
@@ -41,9 +38,7 @@ namespace SEYR.ImageProcessing
                 return Color.FromArgb(255, v, p, q);
         }
 
-        private double Map(double value, double fromLow, double fromHigh, double toLow = 0, double toHigh = 360)
-        {
-            return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
-        }
+        private double Map(double value, double fromLow, double fromHigh, double toLow = 0, double toHigh = 360) => 
+            (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
     }
 }
