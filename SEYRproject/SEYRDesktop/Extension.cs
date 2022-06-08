fafef454e55c3ce59205e93b36f10c19a41699e3
Brad <brad.martin@xdisplay.com>
2022-06-08 09:07:55 -0400

    sync desktop version

--- SEYRproject/SEYRDesktop/Extension.cs
+++ SEYRproject/SEYRDesktop/Extension.cs
@@ -7,28 +7,35 @@ namespace SEYRDesktop
 {
     public static class Extension
     {
-        public static IEnumerable<string> AlphanumericSort(this IEnumerable<string> me)
+        public static IEnumerable<string> AlphanumericSort(this IEnumerable<string> raw)
         {
-            List<(double, double)> result = new List<(double, double)>();
-            foreach (string item in me)
+            try
             {
-                FileInfo fileInfo = new FileInfo(item);
-                string name = fileInfo.Name.Replace(fileInfo.Extension, "");
-                string[] cols = name.Split('_');
-                bool match = double.TryParse(cols[0], out double x);
-                bool match2 = double.TryParse(cols[1], out double y);
-                if (match && match2) result.Add((x, y));
+                List<(double, double)> result = new List<(double, double)>();
+                foreach (string item in raw)
+                {
+                    FileInfo fileInfo = new FileInfo(item);
+                    string name = fileInfo.Name.Replace(fileInfo.Extension, "");
+                    string[] cols = name.Split('_');
+                    bool match = double.TryParse(cols[0], out double x);
+                    bool match2 = double.TryParse(cols[1], out double y);
+                    if (match && match2) result.Add((x, y));
+                }
+                if (result.Count > 0)
+                {
+                    result = result.OrderBy(x => x.Item1).OrderBy(x => x.Item2).ToList();
+                    List<string> output = new List<string>();
+                    foreach ((double, double) r in result)
+                        output.Add(raw.Where(x => x.Contains($"{r.Item1}_{r.Item2}")).First());
+                    return output;
+                }
+                else
+                    return raw.OrderBy(x => Regex.Replace(x, @"\d+", m => m.Value.PadLeft(50, '0')));
             }
-            if (result.Count > 0)
+            catch (System.Exception)
             {
-                result = result.OrderBy(x => x.Item1).OrderBy(x => x.Item2).ToList();
-                List<string> output = new List<string>();
-                foreach ((double, double) r in result)
-                    output.Add(me.Where(x => x.Contains($"{r.Item1}_{r.Item2}")).First());
-                return output;
+                return raw;
             }
-            else
-                return me.OrderBy(x => Regex.Replace(x, @"\d+", m => m.Value.PadLeft(50, '0')));
         }
     }
 }
